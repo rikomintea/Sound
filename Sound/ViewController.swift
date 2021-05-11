4706ee3123d133835ed30fd5010a3f7ee0fa2c16
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-11 17:28:48 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by スマート・ナビ on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
